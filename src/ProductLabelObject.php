9f10d8deedfff273a0c0e24c261189609095e500
Pau Benet Prat <paubenetprat@MacBook-Pro-de-Pau.local>
2017-03-31 16:26:46 +0200

    ProductLabelobjects added, missing Image encoder

---
+++ src/ProductLabelObject.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace RevoSystems\ProductLabel;
+
+
+class ProductLabelObject {
+    public $json;
+    public $values;
+    public function render($json, $values) {
+        $this->json   = $json;
+        $this->values = $values;
+        return "<div style='{$this->getBoxStyle()}'>{$this->getBody()}</div>";
+    }
+
+    public function getBoxSize() {
+        return "left: {$this->json["x"]}px; top: {$this->json["y"]}px; width: {$this->json["width"]}px; height: {$this->json["height"]}px; position: absolute;";
+    }
+    // ABSTRACT METHODS //
+    public function getBoxStyle() {
+        return "{$this->getBoxSize} {$this->getStyle}";
+
+    }
+
+    public function getStyle() {
+        return "";
+    }
+
+    public function getBody() {
+        return "";
+    }
+}
