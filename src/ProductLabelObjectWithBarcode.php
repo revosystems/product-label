f54854372242027bd40db9b066addb08d2ca7e51
Pau Benet Prat <pau@revo.works>
2022-08-02 13:56:05 +0200

    Add try catch dependency.

--- src/ProductLabelObjectWithBarcode.php
+++ src/ProductLabelObjectWithBarcode.php
@@ -3,18 +3,23 @@
 namespace RevoSystems\ProductLabel;
 
 use Picqer\Barcode\BarcodeGeneratorPNG;
+use Picqer\Barcode\Exceptions\InvalidLengthException;
 
 class ProductLabelObjectWithBarcode extends ProductLabelObjectWithText
 {
     public function getBody()
     {
-        return $this->getBarcode($this->json['text']);
+        return $this->getBarcode($this->json['text'] ?? '');
     }
 
     public function getBarcode(string $text)
     {
         $generator    = new BarcodeGeneratorPNG();
-        $barcodeImage = $generator->getBarcode($text, $generator::TYPE_CODE_39E, 1, $this->json['height']);
+        try {
+            $barcodeImage = $generator->getBarcode($text, $generator::TYPE_CODE_39E, 1, $this->json['height']);
+        } catch (InvalidLengthException $e) {
+            return '';
+        }
         return '<img style="height:' . $this->getSize() . ';width:' . $this->json['width'] . 'px;" src="data:image/png;base64,' . base64_encode($barcodeImage) . '">';
     }
 
